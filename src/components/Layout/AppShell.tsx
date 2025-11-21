ee3636cfcf4137229c4c883e05c6dce92003ba02
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-11-21 22:53:30 +0000

    fix mobile layout height

--- src/components/Layout/AppShell.tsx
+++ src/components/Layout/AppShell.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const AppShell: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
-        <div className="h-screen w-screen overflow-hidden bg-black relative font-sans">
+        <div className="h-dvh w-screen overflow-hidden bg-black relative font-sans">
             {children}
         </div>
     );
